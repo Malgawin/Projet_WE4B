331f959dae81187ddc94866cd5f5915d47300259
Malgawin <malgawin39@gmail.com>
2025-05-16 09:49:21 +0200

    Création du form pour créer les depots textes

--- Projet_WE4B/src/app/app.module.ts
+++ Projet_WE4B/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FormCreateTextComponent } from './form-create-text/form-create-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FormCreateTextComponent
   ],
   imports: [
     BrowserModule,
---
+++ Projet_WE4B/src/app/form-create-text/form-create-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-create-text',
+  templateUrl: './form-create-text.component.html',
+  styleUrls: ['./form-create-text.component.css']
+})
+export class FormCreateTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
