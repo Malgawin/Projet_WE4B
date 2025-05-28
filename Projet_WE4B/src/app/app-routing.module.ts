3af9b14f7b1d3789344417aec0ff5c20b7bbb57a
Malgawin <malgawin39@gmail.com>
2025-05-28 09:03:33 +0200

    Ajout route creation texte

--- Projet_WE4B/src/app/app-routing.module.ts
+++ Projet_WE4B/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TableauDeBordComponent } from './tableau-de-bord/tableau-de-bord.component';
+import { FormCreateTextComponent } from './form-create-text/form-create-text.component';
 
 const routes: Routes = [
   { path: 'tableau-de-bord', component: TableauDeBordComponent },
+  { path: 'creation-texte', component: FormCreateTextComponent},
 ];
 
 @NgModule({
